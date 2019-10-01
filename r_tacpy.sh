d8a3216a27037f0b55346d7bd94b3e875b06e8d2
Baron <arthur.baron@epitech.eu>
2019-10-01 22:00:28 +0200

    01/10|22:00|piscineDay02 task 07 corrected

--- r_tacpy.sh
+++ r_tacpy.sh
@@ -3,6 +3,6 @@
 export MY_LINE1=24
 export MY_LINE2=42
 
-cut -d: -f1 | sed '0~2d' | rev | sort -r | sed -n "$MY_LINE1,$MY_LINE2 p" | tr "\n" "," | sed 's/,/, /g' | sed 's/..$/\.\n\n/g'
+cut -d: -f1 | sed '1d; n; d' | rev | sort -r | sed -n "$MY_LINE1,$MY_LINE2 p" | tr "\n" "," | sed 's/,/, /g' | sed 's/..$/\.\n\n/g'
 
 
