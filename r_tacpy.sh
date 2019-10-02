2f09c6714ae2eebf3f3362ab88e6113e96dc4b44
Baron <arthur.baron@epitech.eu>
2019-10-02 09:25:58 +0200

    02/10|09:25|piscineDay02| task 07 corrected

--- r_tacpy.sh
+++ r_tacpy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export MY_LINE1=24
-export MY_LINE2=42
+export MY_LINE1
+export MY_LINE2
 
 cut -d: -f1 | sed '1d; n; d' | rev | sort -r | sed -n "$MY_LINE1,$MY_LINE2 p" | tr "\n" "," | sed 's/,/, /g' | sed 's/..$/\.\n\n/g'
 
